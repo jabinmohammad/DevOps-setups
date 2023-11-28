2feb8882dfd87488dc1112455fc24c06ea1796d7
jabinmohammad <144356021+jabinmohammad@users.noreply.github.com>
2023-11-28 21:17:07 +0530

    Update 1.Kops.sh

--- 1.Kops.sh
+++ 1.Kops.sh
@@ -16,3 +16,5 @@ aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham007.k8s.local --re
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham007.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name rahams.k8s.local --yes --admin
+
+
